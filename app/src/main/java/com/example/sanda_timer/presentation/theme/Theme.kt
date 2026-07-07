573e8491bec4cdb47747f9d0ef375f68edad3d61
ilianoKokoro <iliandelagrange@gmail.com>
2026-07-06 22:51:33 -0400

    chore: create project

---
+++ app/src/main/java/com/example/sanda_timer/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package com.example.sanda_timer.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material3.MaterialTheme
+
+@Composable
+fun SandaTimerTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
